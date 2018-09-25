f71b98f3916901c9e1afea0528a70424d7daa68c
zyiz <1109580034@qq.com>
2018-09-25 13:23:00 +0800

    13. 罗马数字转整数

--- Leetcode/Leetcode/main.swift
+++ Leetcode/Leetcode/main.swift
@@ -10,10 +10,10 @@ import Foundation
 
 print("Hello, World!")
 
-///1. 两数之和
+///1. 两数之和 *
 print(oneSum.twoSum([2,7,11,15], 9))
 
-///2. 两数相加
+///2. 两数相加 **
 ///测试用例 ： 【5】【5】，【1】，【1】，【1，3】
 print("---------------------")
 let l1 = twoListNode.init(val: [2,4,3])
@@ -25,18 +25,26 @@ twoAdd.addTwoNumbers(nil,l11)?.twoPrint()
 let l22 = twoListNode.init(5)
 twoAdd.addTwoNumbers(l22,l22)?.twoPrint()
 
-///3. 无重复字符的最长子串
+///3. 无重复字符的最长子串 **
 print("---------------------")
 print(threeString.lengthOfLongestSubstring("pwwkew"))
 
-///5. 最长回文子串
+///5. 最长回文子串 **
 print("---------------------")
 print(fiveString.longestPalindrome("cbbdc"))
 
 
-///7. 反转整数
+///7. 反转整数 *
 print("---------------------")
 print(sevenReversal.reverse(123))
 
 
+///9. 回文数 *
+print("---------------------")
+print(nineString.isPalindrome(1))
+
+
+///13. 罗马数字转整数 *
+print("---------------------")
+print(thirteenRome.romanToInt("MCMXCIV"))
 
